b7964ec752ccd5985034d8c4ba47412ef7daac11
Claudia Lamas <claudia.c.lamas@gmail.com>
2024-03-28 15:22:23 +0000

    initial commit with some fields of form application

---
+++ src/app/pages/application/page.tsx
@@ -0,0 +1,9 @@
+import { ApplicationForm } from "@/app/ui/application/applicationForm";
+export default function () {
+  return (
+    <>
+      <h1>Application page</h1>
+      <ApplicationForm />
+    </>
+  );
+}
