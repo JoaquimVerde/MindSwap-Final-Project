359bccf109289b617a035f4ed67e35d567cf7d93
JoaquimVerde <joaquim.verde@gmail.com>
2024-04-01 17:33:28 +0100

    all courses and course page

---
+++ src/app/pages/dashboard/all-courses/[id]/course/page.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button";
+import { CircuitBoard, Smile } from "lucide-react";
+
+
+export default async function Page() {
+
+
+    // fazer fetchCourseById (data.ts)
+
+    return (
+        <div className="mx-2 my-2">
+
+
+            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-2 max-w-fit">
+                (Course Name)
+            </h2>
+
+            <div className="my-6 w-full overflow-y-auto">
+                <table className="max-w-fit">
+                    <tbody>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                                Teacher
+                            </td>
+                            <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                                (Teacher Name)
+                            </td>
+                        </tr>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                                Schedule
+                            </td>
+                            <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                                (Course Schedule)
+                            </td>
+                        </tr>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                                Location
+                            </td>
+                            <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                                (Course Location)
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="mx-2 my-10">
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                    Program
+                </h3>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    (Course Program)
+                </p>
+            </div>
+
+            <div className="flex flex-col space-y-5 max-w-fit">
+                <Button>View Syllabus <CircuitBoard className="ml-3" /></Button>
+                <Button>Apply Now <Smile className="ml-3" /></Button>
+            </div>
+
+
+        </div>
+    )
+}
--- src/app/pages/dashboard/all-courses/page.tsx
+++ src/app/pages/dashboard/all-courses/page.tsx
@@ -1,1 +1,112 @@
-// display all available courses
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Scroll } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { GetCourseInfo } from "@/app/ui/courses/buttons";
+
+
+
+
+
+export default async function Page() {
+
+    // fazer fetchCourses(data.ts)
+
+    //const courses = await fetchCourses(query, currentPage);
+
+   
+
+    return (
+        <div className="mx-2 my-2">
+
+            <div>
+                <h1 className='text-2xl'>All Courses</h1>
+            </div>
+
+            {/*<div>
+                {courses?.map((course) => (
+                    <div key={course.id}>           */}  
+
+            
+
+            <Card className={cn("w-[380px] mx-2 my-2")}>
+                <CardHeader>
+                    <CardTitle>(Name)</CardTitle>
+                    <CardDescription>(SmallDescription)</CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <div className=" flex items-center space-x-4 rounded-md border p-4">
+                        <Scroll />
+                        <div className="flex-1 space-y-1">
+                            <p className="text-sm font-medium leading-none">
+                                Price: (price)
+                            </p>
+                            <p className="text-sm text-muted-foreground">
+                                Duration: (duration)
+                            </p>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div
+                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                        >
+                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                            <div className="space-y-1">
+                                <p className="text-sm font-medium leading-none">
+                                    I Module Title:
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                    (Module description)
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                        >
+                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                            <div className="space-y-1">
+                                <p className="text-sm font-medium leading-none">
+                                    II Module Title:
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                    (Module description)
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                        >
+                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                            <div className="space-y-1">
+                                <p className="text-sm font-medium leading-none">
+                                    III Module Title:
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                    (Module description)
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full">
+                        <GetCourseInfo /> {/*id={course.id} */}
+                    </Button>
+                </CardFooter>
+            </Card>
+            {/* </div>
+
+            ))}</div>*/}
+            
+        </div >
+    )
+}
