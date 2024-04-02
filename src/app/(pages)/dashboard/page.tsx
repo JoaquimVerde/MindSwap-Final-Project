1c404866eb3e9d9daabd4a767c1fde100799e6d8
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-02 16:02:23 +0100

    conflicts with origin dev

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,6 +1,5 @@
 // using partial reloading here (nav bar wont change when navigating to other pages)
 
-
 import Sidenav from "../../ui/dashboard/sidenav";
 
 export default function Dashboard() {
@@ -9,8 +8,5 @@ export default function Dashboard() {
 		<p>Dashboard page here</p>
 		<Sidenav />
 	</>
-	
-
-
 	
 }
