c06e4511628eca5e5eaaaf15bd81634c46c51e67
c-cunha <car930193@gmail.com>
2024-04-03 15:13:03 +0100

    import Calendar component to the Dashboard Page
    Co-authored-by: jgmsv <jose.guilherme.vieira@gmail.com>

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,12 +1,13 @@
 // using partial reloading here (nav bar wont change when navigating to other pages)
-
-import Sidenav from "../../ui/dashboard/sidenav";
+import { Calendar } from "@/components/ui/calendar"
 
 export default function Dashboard() {
-	return
-	<>
-		<p>Dashboard page here</p>
-		<Sidenav />
-	</>
+	return(
+
+	<main>
+		<h1>Dashboard page here</h1>
+		<Calendar />
+	</main>
+	)
 	
 }
