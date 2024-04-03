007ddba6095fd87a3640a3f5904e095bab4331a7
melissasa6 <melissasa6@gmail.com>
2024-04-03 15:22:23 +0100

    add all the questions to FAQ

--- src/app/(pages)/faqs/page.tsx
+++ src/app/(pages)/faqs/page.tsx
@@ -3,11 +3,11 @@ import Questions from "@/app/ui/faqs/faqs";
 export default function FaqPage(){
     return (
         <main className="flex items-center justify-center">
-		  <div className="flex-min-h-screen flex-col items-center justify-between p-10">
-          <h1 className="text-3xl text-center font-bold mb-5"> Frequently Asked Questions </h1>
-
-			<Questions />
-		  </div>
-		</main>
+		        <div className="flex-min-h-screen flex-col items-center justify-between p-10">
+              <h1 className="text-3xl text-center font-bold mb-5"> Frequently Asked Questions (FAQ) </h1>
+                <h2 className="text-xl text-center font-bold mb-10">  Need Help? We've got you covered </h2>
+			            <Questions />
+		        </div>
+		    </main>
     )
 }
