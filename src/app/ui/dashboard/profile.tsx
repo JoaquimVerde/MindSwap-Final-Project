93a3651bf55d50c90b35c15b9c81cb6b85382b0a
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2024-04-04 13:59:53 +0100

    insert text fixed for testing field boxes

--- src/app/ui/dashboard/profile.tsx
+++ src/app/ui/dashboard/profile.tsx
@@ -1,45 +1,46 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 const Profile = () => {
-  // Replace these with your actual state variables and update functions
-  const name = "";
-  const username = "";
-  const email = "";
-  const address = "";
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
 
   return (
     <div className="flex flex-col items-start pl-50">
       <h1 className="mb-10 text-slate-400">Profile</h1>
-      <div className="flex flex-col justify-evenly w-full ml-6">
+      <div className="flex flex-col justify-evenly w-full">
         <div>
           <p className="font-bold text-slate-400 mt-4">Name:</p>
           <input
             type="text"
             value={name}
-            onChange={(e) => {}}
-            className="rounded"
+            onChange={(e) => setName(e.target.value)}
+            className="rounded w-80"
           />
           <p className="font-bold text-slate-400 mt-4">Username:</p>
           <input
             type="text"
             value={username}
-            onChange={(e) => {}}
-            className="rounded"
+            onChange={(e) => setUsername(e.target.value)}
+            className="rounded w-80"
           />
           <p className="font-bold text-slate-400 mt-4">Email:</p>
           <input
             type="text"
             value={email}
-            onChange={(e) => {}}
-            className="rounded"
+            onChange={(e) => setEmail(e.target.value)}
+            className="rounded w-80"
           />
           <p className="font-bold text-slate-400 mt-4">Address:</p>
           <input
             type="text"
             value={address}
-            onChange={(e) => {}}
-            className="rounded"
+            onChange={(e) => setAddress(e.target.value)}
+            className="rounded w-80"
           />
         </div>
         <Button className="w-32 hover:bg-blue-700 mt-8">Edit Profile</Button>
