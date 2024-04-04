26a75964f5062649e19ac023bc308395ca9c89a2
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2024-04-04 15:39:24 +0100

    added text box to each field and enabled saving text. Added placeholder-data to some fields in order to test

--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -1,10 +1,12 @@
-// // fake data to test data fetching methods and how forms are working
+// fake data to test data fetching methods and how forms are working
 
-// const students = [
-//      {
-//           id: '410544b2-4001-4271-9855-fec4b6a6442a',
-//           name: 'User',
-//           email: 'user@nextmail.com',
-//           password: '123456',
-//      },
-// ];
+const students = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "User",
+    email: "user@nextmail.com",
+    password: "123456",
+  },
+];
+
+export default students;
