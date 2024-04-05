aa316b74e163c4bc9077f44a4af3e1a85ffc697b
c-cunha <car930193@gmail.com>
2024-04-05 10:35:53 +0100

    inspirational quotes card and next quote button position fixed.
    Co-authored-by: jgmsv <jgmsv@users.noreply.github.com>

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -5,32 +5,30 @@ import Time from "@/app/ui/dashboard/time";
 import InspirationalQuotes from "@/app/ui/dashboard/inspirational-quotes";
 
 export default function Dashboard() {
-	return (
-		<main>
-			<h1>Dashboard</h1>
-			<div className="flex flex-row-reverse justify-between mt-10 gap-3">
-				<Calendar />
-				<div className="card w-116 bg-base-100 shadow-xl">
-					<div className="card-body bg-white rounded-2xl text-base">
-						<div className="card-actions justify-end"></div>
-						<div className="flex justify-center flex-wrap sm:block">
-							<InspirationalQuotes />
-						</div>
-					</div>
-				</div>
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <div className="flex flex-row-reverse justify-between mt-10 gap-3">
+        <Calendar />
+        <div className="card w-118 bg-base-100 shadow-xl">
+          <div className="card-body bg-white rounded-2xl text-base">
+            <div className="card-actions justify-end"></div>
+            <div className="flex justify-center flex-wrap sm:block">
+              <InspirationalQuotes />
+            </div>
+          </div>
+        </div>
 
-				<div className="card w-96 bg-base-100 shadow-xl">
-					<div className="card-body bg-white rounded-2xl text-base">
-						<div className="card-actions justify-end"></div>
-						<div className="flex justify-center flex-wrap sm:block text-center">
-							<h2 className="pb-10 pt-10">
-								Welcome Person
-							</h2>
-							<Time />
-						</div>
-					</div>
-				</div>
-			</div>
-		</main>
-	);
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body bg-white rounded-2xl text-base">
+            <div className="card-actions justify-end"></div>
+            <div className="flex justify-center flex-wrap sm:block text-center">
+              <h2 className="pb-10 pt-10">Welcome Person</h2>
+              <Time />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
