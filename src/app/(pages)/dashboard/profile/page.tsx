a2e37d627dc8bef1a52ee67736ce436a310c2694
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2024-04-05 10:47:05 +0100

    added avatar to profile

--- src/app/(pages)/dashboard/profile/page.tsx
+++ src/app/(pages)/dashboard/profile/page.tsx
@@ -3,13 +3,20 @@
 // change email - only if student?
 // change photo
 
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import Profile from "@/app/ui/dashboard/profile";
 import React from "react";
 
 const ProfilePage = () => {
   return (
-    <div>
+    <div className="flex">
       <Profile />
+      <div className="border border-gray-300 w-52 h-52 ml-10 rounded-full overflow-hidden">
+        <Avatar className="w-full h-full rounded-full bg-gray-300">
+          <AvatarImage src="/images/avatar.png" />
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 };
