e454d6aebf2e27f6ff831e5b899a683e9268b09a
melissasa6 <melissasa6@gmail.com>
2024-04-05 16:46:14 +0100

    Add an icon to the personal space on the dashboard. Remove login and signup from dashboard navbar

--- src/app/ui/dashboard/Navbar.tsx
+++ src/app/ui/dashboard/Navbar.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { LogOut } from "lucide-react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CircleUserRound } from "lucide-react";
 
 
 export default function Navbar() {
@@ -64,12 +65,10 @@ export default function Navbar() {
       
         <div className="flex flex-col items-center justify-between">
             <div className="flex flex: row items-center justify-between">
-            <Avatar>
-                <AvatarImage src="/images/robot.png" onClick={toggleMenu} />
-            </Avatar>
+            <CircleUserRound className="size-12" color="#ffffff" onClick={toggleMenu} /> 
             </div>
             {isMenuOpen && (
-            <div className="flex flex-col  absolute right-8 mt-10 w-48 bg-white border rounded-lg shadow-lg z-50">
+            <div className="flex flex-col  absolute right-8 mt-12 w-48 bg-white border rounded-lg shadow-lg z-50">
                  <h2 className="px-15 py-5 text-sm text-gray-1000 flex justify-center">{fullName}</h2>
 
                 <Link
