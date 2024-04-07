7565f0e7d13f3339764bc474dfe26b0295ea02bf
c-cunha <car930193@gmail.com>
2024-04-07 01:32:58 +0100

    handleToggle with DOM event first try (sidenav - hidden).
    Co-authored-by: jgmsv <jose.guilherme.vieira@gmail.com>

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { Menu, X } from 'lucide-react';
 
-interface SidenavProps {
-  isCollapsed: boolean;
-  toggleCollapse: () => void;
-}
+export default function Sidenav() {
+  const handleToggle = () => {
+    const sidenav = document.querySelector<HTMLDivElement>('.drawer');
+    if (sidenav) {
+      sidenav.classList.toggle('hidden');
+    }
+  };
 
-export default function Sidenav({ isCollapsed, toggleCollapse }: SidenavProps) {
   return (
-    <div className={`drawer lg:drawer-open ${isCollapsed ? 'hidden' : ''}`}>
+    <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-row items-start justify-start ml-5 mt-5 ">
         <label
@@ -22,14 +24,12 @@ export default function Sidenav({ isCollapsed, toggleCollapse }: SidenavProps) {
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content text-sm font-medium">
-          <li onClick={toggleCollapse}>
-            <X size="54" className={`cursor-pointer ${isCollapsed ? 'flex-row-reverse' : 'flex-row'}`} />
+          <li onClick={handleToggle}>
+            <X size="54" className="cursor-pointer flex-row" />
           </li>
-          {!isCollapsed && (
-            <li>
-              <NavLinks />
-            </li>
-          )}
+          <li>
+            <NavLinks />
+          </li>
         </ul>
       </div>
     </div>
