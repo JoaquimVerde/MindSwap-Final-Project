a8eeabd285dbc4ec5c06f8591602782a12b03960
FilipeMPinto91 <152388850+FilipeMPinto91@users.noreply.github.com>
2024-04-08 18:18:04 +0100

    Fetching data and displaying on profile using a static userId

--- src/app/(pages)/dashboard/profile/page.tsx
+++ src/app/(pages)/dashboard/profile/page.tsx
@@ -1,17 +1,12 @@
 // profile page w/ settings that you can change, depending on your role permissions
-// display name
-// change email - only if student?
-// change photo
 
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import Profile from "@/app/ui/dashboard/profile";
 import React from "react";
-import {Camera} from "lucide-react";
 
 const ProfilePage = () => {
   return (
     <div className="flex-min-h-screen flex-col items-center justify-between">
-      <Profile />
+      <Profile username={""} id={""} email={""} firstName={""} lastName={""} address={""} />
     </div>
   );
 };
