3126df20867d0fbba7ef4c58be6679d056c7930c
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-08 22:01:45 +0100

    barabara

--- src/app/(pages)/signUp/page.tsx
+++ src/app/(pages)/signUp/page.tsx
@@ -3,7 +3,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import SessionWrapper from "../../../components/ui/session-wrapper";
 
-function Login() {
+function SignUp() {
   const { data: session } = useSession();
   const router = useRouter();
 
@@ -16,7 +16,10 @@ function Login() {
       <div className="w-full h-screen flex flex-col justify-center items-center">
         <button
           className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2"
-          onClick={() => signIn("cognito")}
+          onClick={() =>
+            (window.location.href =
+              "https://mindswaplogin.auth.eu-central-1.amazoncognito.com/signup?client_id=3qn4vfitu65korf49pnrp1pt24&response_type=code&scope=email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fcognito")
+          }
         >
           Cognito
         </button>
@@ -24,12 +27,12 @@ function Login() {
     );
   }
 }
-const LoginWithSession = () => {
+const SignUpWithSession = () => {
   return (
     <SessionWrapper>
-      <Login />
+      <SignUp />
     </SessionWrapper>
   );
 };
 
-export default LoginWithSession;
+export default SignUpWithSession;
---
+++ src/app/api/auth/callback/cognito/page.tsx
@@ -0,0 +1,8 @@
+function Redirect() {
+    //TODO verificar se utilizador é um novo user ou não.
+    // Se for mandar para o /register;
+    // Se não for mandar para o /dashboard;
+    return <div>Redirecting...</div>;
+}
+
+export default Redirect;
