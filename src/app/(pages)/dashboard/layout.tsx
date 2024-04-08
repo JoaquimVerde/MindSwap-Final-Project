946f0e412f7983901a332af3a44eeaeb4528c79e
c-cunha <car930193@gmail.com>
2024-04-08 14:51:15 +0100

    commit

--- src/app/(pages)/dashboard/layout.tsx
+++ src/app/(pages)/dashboard/layout.tsx
@@ -1,30 +1,30 @@
 
 'use client';
-
+// Layout.tsx
 import React, { useState } from 'react';
 import Sidenav from '@/app/ui/dashboard/sidenav';
 import Navbar from "@/app/ui/dashboard/Navbar";
+import { Menu } from 'lucide-react';
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isSidenavOpen, setIsSidenavOpen] = useState(true);
 
-export default function Layout({ children }: LayoutProps) {
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const toggleCollapse = () => {
-    setIsCollapsed(!isCollapsed);
+  const toggleSidenav = () => {
+    setIsSidenavOpen(!isSidenavOpen);
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-white">
       {/* Sidebar */}
-      <div className={`flex-none md:w-64 ${isCollapsed ? 'hidden' : ''}`}>
-        <Sidenav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} />
+      <div className={`flex-none md:w-64 ${isSidenavOpen ? '' : 'hidden'}`}>
+        {/* Pass onHide as prop */}
+        <Sidenav isHidden={!isSidenavOpen} onHide={toggleSidenav} />
       </div>
 
       {/* Main Content */}
-      <div className={`flex-grow p-6 md:overflow-y-auto md:p-12 z-20 ml-10 ${isCollapsed ? 'w-full' : 'md:w-auto'}`}>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 z-20 ml-10">
+        {/* Toggle button */}
+        <Menu onClick={toggleSidenav} size="24" className='bg-white'/>
         {children}
       </div>
     </div>
