3062b8960241c198cb0483c8155c007995c0890c
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-09 17:15:15 +0100

    my-courses-teacher/page add empty string as placeholder

--- src/app/(pages)/dashboard/my-courses-teacher/page.tsx
+++ src/app/(pages)/dashboard/my-courses-teacher/page.tsx
@@ -4,7 +4,7 @@ export default function MyCoursesTeacher() {
     return (
         <div>
             <h1>My Courses - Teacher</h1>
-            <SearchBar />
+            <SearchBar placeholder={""}/>
         </div>
     );
 }
