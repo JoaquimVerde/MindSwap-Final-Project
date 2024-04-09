d5e1008d96fd5df81df6813d0ff6866f88ed441c
barbara-rolim <barbaraerolim@gmail.com>
2024-04-09 11:52:11 +0100

    continue Login

--- src/app/api/auth/callback/cognito/page.tsx
+++ src/app/api/auth/callback/cognito/page.tsx
@@ -1,8 +1,28 @@
+import { useRouter } from "next/navigation";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+
 function Redirect() {
-    //TODO verificar se utilizador é um novo user ou não.
-    // Se for mandar para o /register;
-    // Se não for mandar para o /dashboard;
-    return <div>Redirecting...</div>;
+    const router = useRouter();
+    const verifyUser = async () => {
+        const response = await fetch('http://localhost:8080/api/v1/persons/email/' + email);
+        response.status === 200 ? router.push("/dashboard") : router.push("/register");
+    }
+}
+
+// Verifier that expects valid access tokens:
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: "<user_pool_id>",
+  tokenUse: "access",
+  clientId: "<client_id>",
+});
+
+try {
+  const payload = await verifier.verify(
+    "eyJraWQeyJhdF9oYXNoIjoidk..." // the JWT as string
+  );
+  console.log("Token is valid. Payload:", payload);
+} catch {
+  console.log("Token not valid!");
 }
 
 export default Redirect;
