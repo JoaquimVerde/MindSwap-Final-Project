0fcf02fbd044edd7e35bd6d77a88121e014313f8
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-10 12:25:06 +0100

    fuck this shit

--- src/app/ui/landingPage/Navbar.tsx
+++ src/app/ui/landingPage/Navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
@@ -66,16 +66,24 @@ function Navbar() {
         <div className="lg:block hidden py-3">
           <button
             className=" px-4 py-1 border-slate-300 text-slate-300 bg-blue-950 hover:bg-blue-700 transition-all duration-300 rounded-md"
-            onClick={() => signIn("cognito")}
+            onClick={() =>
+              signIn("cognito", {
+                callbackUrl: "http://localhost:3000/api/auth/redirect",
+              })
+            }
           >
             login
           </button>
 
-          
-            <button className=" mx-4 px-1 py-1 border-slate-300 text-slate-300 bg-blue-900 hover:bg-blue-600 transition-all duration-300 rounded-md"
-            onClick={() => window.location.href = "https://mindswaplogin.auth.eu-central-1.amazoncognito.com/signup?client_id=3qn4vfitu65korf49pnrp1pt24&response_type=code&scope=email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fcognito"}>
-              Sign Up
-            </button>
+          <button
+            className=" mx-4 px-1 py-1 border-slate-300 text-slate-300 bg-blue-900 hover:bg-blue-600 transition-all duration-300 rounded-md"
+            onClick={() =>
+              (window.location.href =
+                "https://mindswaplogin.auth.eu-central-1.amazoncognito.com/signup?client_id=3qn4vfitu65korf49pnrp1pt24&response_type=code&scope=email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fcognito")
+            }
+          >
+            Sign Up
+          </button>
         </div>
         {/*menu btn for sm devices */}
 
