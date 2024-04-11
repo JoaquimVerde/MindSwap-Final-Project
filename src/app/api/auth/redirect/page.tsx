53d74870d22f9e1efbf1ddc1900302819f2c5616
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-11 13:21:26 +0100

    commit

--- src/app/api/auth/redirect/page.tsx
+++ src/app/api/auth/redirect/page.tsx
@@ -5,11 +5,11 @@ import { useEffect } from "react";
 function Redirect() {
   const router = useRouter();
   const { data: session, status } = useSession();
-  const URL = process.env.API_URL + "/api/v1/persons/email/";
+  const URL = process.env.NEXT_PUBLIC_API_URL + "/api/v1/persons/email/";
 
   useEffect(() => {
     if (status === "loading") return;
-    console.log(process.env.NEXT_PUBLIC_NEXT_SECRET);
+    console.log(process.env.NEXT_PUBLIC_API_URL);
     const verifyUser = async () => {
       const response = await fetch(URL + session?.user?.email);
       const data = await response.json();
--- src/app/ui/register/register-form.tsx
+++ src/app/ui/register/register-form.tsx
@@ -57,7 +57,7 @@ export default function RegisterForm() {
       role: "Student",
     };
     
-    const response = await fetch(process.env.API_URL + "/api/v1/persons", {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/persons", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
