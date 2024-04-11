c36929166f27695a1dfa3f7a786cbf0093899aa2
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-11 15:17:30 +0100

    dsf

--- src/app/ui/register/register-form.tsx
+++ src/app/ui/register/register-form.tsx
@@ -57,7 +57,7 @@ export default function RegisterForm() {
       role: "Student",
     };
     
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/persons", {
+    const response = await fetch("https://fe-deployment-testing.d63irou4ibhxm.amplifyapp.com/proxy/api/v1/persons/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
