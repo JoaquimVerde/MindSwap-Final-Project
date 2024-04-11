0d29af51fa3ac2aeb6f83975edf9ca07d6b17948
melissasa6 <melissasa6@gmail.com>
2024-04-11 10:57:26 +0100

    fetch projects by courseId and  projectsbyId

--- src/app/(pages)/dashboard/all-courses/[id]/projects/projectInfo/page.tsx
+++ src/app/(pages)/dashboard/all-courses/[id]/projects/projectInfo/page.tsx
@@ -1,6 +1,54 @@
-export default function Page (){
-    return(
+import { notFound } from "next/navigation";
+import { fetchProjectById } from "@/app/lib/data";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+
+export default async function Page({ params }: { params: { id: string }}) {
+
+    const id = params.id;
+
+    const project = await Promise.all([
+        (fetchProjectById(id))
+    ]);
+
+
+    if (!project) {
+        notFound();
+    };
+
+
+    return (
         <div>
-             <h1>Project Info</h1>
-        </div>)
+            <h2 className= "text-5xl text-center font-bold">
+                {project[0]?.name}
+            </h2>
+                <Table>
+                    <TableCaption>List of students who participated in the {project[0]?.name} project</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[200px]">First Name</TableHead>
+                                <TableHead>Last Name</TableHead>
+                                <TableHead>Email</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {project[0]?.students?.map((student) => ( 
+                            <TableRow key={student.id}>
+                                 <TableCell className="font-medium">{student.firstName} </TableCell>
+                                 <TableCell>{student.lastName}</TableCell>
+                                 <TableCell>{student.email}</TableCell>
+                            </TableRow> 
+                    ))}
+                        </TableBody>
+                </Table>
+    
+        </div>
+    );
 }
