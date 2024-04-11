68ef5483ec5da76da369e5fbea5b1c91fbdd48ff
FilipeMPinto91 <152388850+FilipeMPinto91@users.noreply.github.com>
2024-04-11 11:15:10 +0100

    Just used prettier to format text

--- src/app/(pages)/privacy-policy/page.tsx
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -1,16 +1,16 @@
-import BackButton from '@/app/ui/components/ui/back-button'
-import React from 'react'
+import BackButton from "@/app/ui/components/ui/back-button";
+import React from "react";
 
 const PrivacyPolicy = () => {
   return (
     <>
-          <div>
-            <BackButton />
-            <h1 className='text-center p-6'>PrivacyPolicy</h1>
-          </div>
+      <div>
+        <BackButton />
+        <h1 className="text-center p-6">PrivacyPolicy</h1>
+      </div>
       <p></p>
     </>
-  )
-}
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
--- src/app/ui/components/ui/back-button.tsx
+++ src/app/ui/components/ui/back-button.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 const BackButton = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   const goBack = () => {
     router.back();
