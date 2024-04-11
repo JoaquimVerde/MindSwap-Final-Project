7475e5358884a4e307c6075185e627b793131b19
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-04-11 21:27:33 +0100

    resolved conflicts rebasing development

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -212,7 +212,6 @@ export async function fetchApplicationById(id: string): Promise<Application> {
   }
 }
 
-<<<<<<< HEAD
 export async function fetchProjectById(id: string): Promise<Project> {
   noStore();
   try {
@@ -248,7 +247,6 @@ export async function fetchProjectByStudentId(id: string): Promise<Project[]> {
     throw new Error("Failed to fetch project.");
   }
 }
-=======
 export async function fetchAllCoursesFromLocation(location : string): Promise<number> {
   noStore();
   try {
@@ -266,4 +264,19 @@ export async function fetchAllCoursesFromLocation(location : string): Promise<nu
   }
 }
 
->>>>>>> a6eeee5 (trying to fix search-bar)
+export async function getPersonByRole(role: string): Promise<Person[]> {
+  noStore();
+  try {
+    const response = await fetch(`http://localhost:3000/proxy/api/v1/persons/role/${role}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch persons");
+    }
+    const persons: Person[] = await response.json();
+    console.log(persons);
+
+    return persons;
+  } catch (error) {
+    console.error("Error fetching persons:", error);
+    return [];
+  }
+}
