c88b269607c7ad760d1a2453776411732f181764
FilipeMPinto91 <152388850+FilipeMPinto91@users.noreply.github.com>
2024-04-11 11:41:25 +0100

    Added BackButton to faqs and contacts

--- src/app/(pages)/faqs/page.tsx
+++ src/app/(pages)/faqs/page.tsx
@@ -1,13 +1,23 @@
+import BackButton from "@/app/ui/components/ui/back-button";
 import Questions from "@/app/ui/faqs/faqs";
 
-export default function FaqPage(){
-    return (
-        <main className="flex items-center justify-center">
-		    <div className="flex-min-h-screen flex-col items-center justify-between p-10">
-              <h1 className="text-3xl text-center font-bold mb-5"> Frequently Asked Questions (FAQ) </h1>
-                <h2 className="text-xl text-center font-bold mb-10">  Need Help? We've got you covered! </h2>
-			        <Questions />
-		    </div>
-		</main>
-    )
+export default function FaqPage() {
+  return (
+    <main className="flex items-center justify-center">
+      <div className="flex-min-h-screen flex-col items-center justify-between p-10">
+        <div className="flex flex-row items-center mb-4">
+          <BackButton />
+          <h1 className="text-3xl flex-grow text-center font-bold mb-5">
+            {" "}
+            Frequently Asked Questions (FAQ){" "}
+          </h1>
+        </div>
+        <h2 className="text-xl text-center font-bold mb-10">
+          {" "}
+          Need Help? We've got you covered!{" "}
+        </h2>
+        <Questions />
+      </div>
+    </main>
+  );
 }
