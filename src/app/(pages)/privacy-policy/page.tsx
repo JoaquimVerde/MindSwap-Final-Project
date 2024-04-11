17bbadea8859ec26dcd1b79fcaf6959f3a651a54
FilipeMPinto91 <152388850+FilipeMPinto91@users.noreply.github.com>
2024-04-11 10:49:32 +0100

    Created Privacy Policy Page

---
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <h1 className='text-center p-6'>PrivacyPolicy</h1>
+      <p></p>
+    </>
+  )
+}
+
+export default PrivacyPolicy
