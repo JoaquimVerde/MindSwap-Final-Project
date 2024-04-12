760517628327da5cb1d834ea3dd5d4c5cfa4b9a5
FilipeMPinto91 <152388850+FilipeMPinto91@users.noreply.github.com>
2024-04-12 15:33:33 +0100

    Added Dropdown Menu with radio item to select role in Add Staff

--- src/app/(pages)/dashboard/all-staff/add-staff/page.tsx
+++ src/app/(pages)/dashboard/all-staff/add-staff/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,9 +13,16 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
-import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
 import React from "react";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function AddStaff() {
   const { toast } = useToast();
@@ -48,20 +54,20 @@ export default function AddStaff() {
     try {
       // Validate the form values
       const validatedValues = formSchema.parse(values);
-      const url = "http://localhost:3000/proxy/api/v1/persons"
-  
+      const url = "http://localhost:3000/proxy/api/v1/persons";
+
       // Add additional fields
-  
-      const api_req_options = {
+
+      const api_req_SelectItems = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(validatedValues),
       };
-  
-      const response = await fetch(url, api_req_options);
-  
+
+      const response = await fetch(url, api_req_SelectItems);
+
       if (response.ok) {
         toast({
           title: "Staff was created successfully",
@@ -81,62 +87,74 @@ export default function AddStaff() {
     }
   }
 
-  
-  
   return (
     <div>
-        <h1>Create new staff</h1>
-<main>
-      <div className="flex flex-col items-center justify-center p-4">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col space-y-20 p-20 pt-0"
-          >
-            <div className="grid w-full max-w-sm items-center gap-3">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="text-input"
-                        placeholder="Email"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="role"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Role</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="text-input"
-                        placeholder="ADMIN or TEACHER"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" className="w-full">
-                Submit
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </div>
-    </main>
+      <h1>Create new staff</h1>
+      <main>
+        <div className="flex flex-col items-center justify-center p-4">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col space-y-20 p-20 pt-0"
+            >
+              <div className="grid w-full max-w-sm items-center gap-3">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-gray-400">Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="text-input"
+                          placeholder="Email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="role"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-gray-400">Role: </FormLabel>
+                      <FormControl>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger className="text-gray-400">
+                            {field.value ? `${field.value} ` : "Choose Role "}
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent>
+                            <DropdownMenuLabel>Role:</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuRadioGroup
+                              value={field.value}
+                              onValueChange={(value) => field.onChange(value)}
+                            >
+                              <DropdownMenuRadioItem value="Teacher">
+                                Teacher
+                              </DropdownMenuRadioItem>
+                              <DropdownMenuRadioItem value="Admin">
+                                Admin
+                              </DropdownMenuRadioItem>
+                            </DropdownMenuRadioGroup>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="w-full my">
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </main>
     </div>
-    
   );
 }
